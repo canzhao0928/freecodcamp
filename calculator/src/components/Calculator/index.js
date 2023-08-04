c34bcaa34860ace7bcb689b186b482db7cdc8473
Can Zhao <zhaocan0928@gmail.com>
2023-08-04 21:42:28 +1000

    finish calculator

--- calculator/src/components/Calculator/index.js
+++ calculator/src/components/Calculator/index.js
@@ -1,20 +1,93 @@
+import { useState } from "react";
+
 export default function Calculator() {
+  const [formula, setFormula] = useState("123");
+  const [lastNum, setLastNum] = useState("");
+  const handleClear = () => {
+    setFormula("");
+  };
+
+  const handleClick = (e) => {
+    if (
+      e.target.innerText === "+" ||
+      e.target.innerText === "-" ||
+      e.target.innerText === "*" ||
+      e.target.innerText === "/"
+    ) {
+      setLastNum("");
+    } else {
+      setLastNum(lastNum + e.target.innerText);
+    }
+    if (e.target.innerText === "." && lastNum.includes(".")) {
+      return;
+    }
+    setFormula(formula + e.target.innerText);
+  };
+  const handleCal = () => {
+    const nums = formula.split(/[+|*|/|-]/).filter((num) => num !== "");
+    let operator = formula.split(/[0-9|.]+/).filter((op) => op !== "");
+
+    operator = operator.map((op) => {
+      if (op.length > 1) {
+        console.log("in");
+        op = op.slice(op.length - 1);
+        console.log(op);
+      }
+      return op;
+    });
+    console.log(nums);
+    console.log(operator);
+    let newFormula = "";
+    for (let i = 0; i < nums.length; i++) {
+      if (nums.length === operator.length) {
+        newFormula += operator[i] + nums[i];
+      } else {
+        if (i === nums.length - 1) {
+          newFormula += nums[i];
+        } else {
+          newFormula += nums[i] + operator[i];
+        }
+      }
+    }
+    let result = eval(newFormula);
+    const str = String(result);
+    if (str.includes(".") && str.split(".")[1].length > 4) {
+      result = result.toFixed(4);
+    }
+    setFormula(result);
+  };
   return (
     <div className="container mt-5" style={{ width: "600px" }}>
+      <div className="row">
+        <input
+          className="text-end fs-1"
+          style={{ height: "100px" }}
+          id="display"
+          type="text"
+          value={formula}
+          readOnly
+        />
+      </div>
       <div className="row">
         <button
           className="col-6 btn btn-outline-secondary rounded-0"
           id="clear"
           style={{ height: "100px" }}
+          onClick={handleClear}
         >
           AC
         </button>
-        <button className="col btn btn-outline-secondary rounded-0" id="divide">
+        <button
+          className="col btn btn-outline-secondary rounded-0"
+          id="divide"
+          onClick={handleClick}
+        >
           /
         </button>
         <button
           className="col btn btn-outline-secondary rounded-0"
           id="multiply"
+          onClick={handleClick}
         >
           *
         </button>
@@ -24,18 +97,28 @@ export default function Calculator() {
           className="col btn btn-outline-secondary rounded-0"
           id="seven"
           style={{ height: "100px" }}
+          onClick={handleClick}
         >
           7
         </button>
-        <button className="col btn btn-outline-secondary rounded-0" id="eight">
+        <button
+          className="col btn btn-outline-secondary rounded-0"
+          id="eight"
+          onClick={handleClick}
+        >
           8
         </button>
-        <button className="col btn btn-outline-secondary rounded-0" id="nine">
+        <button
+          className="col btn btn-outline-secondary rounded-0"
+          id="nine"
+          onClick={handleClick}
+        >
           9
         </button>
         <button
           className="col btn btn-outline-secondary rounded-0"
           id="subtract"
+          onClick={handleClick}
         >
           -
         </button>
@@ -45,16 +128,29 @@ export default function Calculator() {
           className="col btn btn-outline-secondary rounded-0"
           id="four"
           style={{ height: "100px" }}
+          onClick={handleClick}
         >
           4
         </button>
-        <button className="col btn btn-outline-secondary rounded-0" id="five">
+        <button
+          className="col btn btn-outline-secondary rounded-0"
+          id="five"
+          onClick={handleClick}
+        >
           5
         </button>
-        <button className="col btn btn-outline-secondary rounded-0" id="six">
+        <button
+          className="col btn btn-outline-secondary rounded-0"
+          id="six"
+          onClick={handleClick}
+        >
           6
         </button>
-        <button className="col btn btn-outline-secondary rounded-0" id="add">
+        <button
+          className="col btn btn-outline-secondary rounded-0"
+          id="add"
+          onClick={handleClick}
+        >
           +
         </button>
       </div>
@@ -65,18 +161,21 @@ export default function Calculator() {
               className="col btn btn-outline-secondary rounded-0"
               id="one"
               style={{ height: "100px" }}
+              onClick={handleClick}
             >
               1
             </button>
             <button
               className="col btn btn-outline-secondary rounded-0"
               id="two"
+              onClick={handleClick}
             >
               2
             </button>
             <button
               className="col btn btn-outline-secondary rounded-0"
               id="three"
+              onClick={handleClick}
             >
               3
             </button>
@@ -86,12 +185,14 @@ export default function Calculator() {
               className="col-8 btn btn-outline-secondary rounded-0"
               id="zero"
               style={{ height: "100px" }}
+              onClick={handleClick}
             >
               0
             </button>
             <button
               className="col btn btn-outline-secondary rounded-0"
               id="decimal"
+              onClick={handleClick}
             >
               .
             </button>
@@ -100,6 +201,7 @@ export default function Calculator() {
         <button
           className="col-3 btn btn-outline-secondary rounded-0"
           id="equals"
+          onClick={handleCal}
         >
           =
         </button>
